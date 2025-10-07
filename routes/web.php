cf089edc53f6ee4cbd06c2475a91fd1b47ed928b
Reyhandhani <reyhandhani11@gmail.com>
2025-10-07 11:37:15 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/anime', function () {
-    return view('anime'); // akan mencari resources/views/anime.blade.php
+    return view('anime');
 });
